52133434ec7ccb1c2654a3a7f21c2f65748aae6a
BhanuprasadTsg <81859414+BhanuprasadTsg@users.noreply.github.com>
2021-06-23 21:32:49 +0530

    Delete marvrlheros directory

--- marvrlheros/marvel hero random/marvel1.js
+++
@@ -1,7 +0,0 @@
-
-var z=document.getElementById("btn");
-z.addEventListener("click",function(){
-    document.getElementById("marvel").innerHTML="bhanu";
-    document.getElementById("marvel1").innerHTML="prince";
-
-});
