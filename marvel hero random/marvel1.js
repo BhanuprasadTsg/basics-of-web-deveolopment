efc426a20e37033a9d33906e37b658475aa39023
BhanuprasadTsg <81859414+BhanuprasadTsg@users.noreply.github.com>
2021-06-23 21:33:21 +0530

    Add files via upload

---
+++ marvel hero random/marvel1.js
@@ -0,0 +1,7 @@
+
+var z=document.getElementById("btn");
+z.addEventListener("click",function(){
+    document.getElementById("marvel").innerHTML="bhanu";
+    document.getElementById("marvel1").innerHTML="prince";
+
+});
